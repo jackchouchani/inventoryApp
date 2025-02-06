68d46ec76822075aab62a58de99d49f4cad3eeec
Jack <jack.chouchani@epita.fr>
2025-02-06 18:20:03 +0100

    fixing another bug

--- app.config.ts
+++ app.config.ts
@@ -19,7 +19,9 @@ export default {
           disableTutorial: true,
           origin: false
         },
-        EXPO_OS: 'web'
+        EXPO_OS: 'web',
+        SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
+        SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
       },
       plugins: [
         [
--- src/config/supabase.tsx
+++ src/config/supabase.tsx
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
+
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   throw new Error('Les variables d\'environnement Supabase ne sont pas définies');
