564f8d404f888a508df02763c4025527e289cbcf
Jack Chouchani <jack.chouchani@epita.fr>
2025-05-21 17:10:06 +0200

    Ajout d'une configuration pour utiliser Google Fonts avec Material Icons sur la plateforme web. Cela permet d'améliorer l'affichage des icônes dans l'application web en remplaçant la police par défaut.

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
 import "expo-router/entry";
+import { Platform } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+// Override MaterialIcons to use Google Fonts on web
+if (Platform.OS === 'web') {
+  MaterialIcons.font = {
+    fontFamily: 'Material Icons',
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+  };
+}
