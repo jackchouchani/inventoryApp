119b2ea5ee06eb6122951783dd733b1399ca6a77
Jack Chouchani <jack.chouchani@epita.fr>
2025-06-03 23:09:46 +0200

    🔄 Mise à jour PWA v1.4.0

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.3.0'; // Synchronisé avec app.json
+const APP_VERSION = '1.4.0'; // Synchronisé avec app.json
 const CACHE_NAME = `inventory-app-cache-v${APP_VERSION}`;
 const STATIC_ASSETS = [
   '/',
